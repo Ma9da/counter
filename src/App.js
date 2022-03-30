37a938cca8b10ea14ce209a866c04be87f9d52e0
ma9da <dodymasry25@gmail.com>
2022-03-30 13:17:23 +0200

    counter added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import { useRef, useState } from 'react';
 function App() {
+  const [counter, setCounter] = useState(0)
   return (
     <>
+      <button onClick={() => setCounter(counter + 1)}>increase +1</button>
+      <button onClick={() => setCounter(counter + 2)}>increase +2</button>
+      <div className={counter > 10 ? 'green' : ''}>
+        counter: {counter >= 0 ? counter : 0}
+        {counter <= 5 && counter > 0 ? <div className='red'>your counter is lower than 5</div> : ""}
+      </div>
+      <button onClick={() => setCounter(counter - 1)}>decrease -1</button>
+      <button onClick={() => setCounter(counter - 2)}>increase -2</button>
     </>
   );
 }
